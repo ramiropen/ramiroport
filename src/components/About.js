57ba306bf4d780ecf25e548452a504ad51471430
unknown <ramiropen.work@gmail.com>
2022-04-28 21:47:43 -0700

    fixed deploy

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Accordion from "react-bootstrap/Accordion";
 
+import {FaGitHubSquare} 
+
 export default function About() {
   const wrapperStyle = {
     textAlign: "center",

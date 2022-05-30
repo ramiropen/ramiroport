cf8b780f13bbb771d0f480c4c7359407af0cfbfc
unknown <ramiropen.work@gmail.com>
2022-05-30 13:25:35 -0700

    css changes and added a carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,14 +8,13 @@ export default function Home() {
   const wrapperStyle = { textAlign: "center", marginTop: "100px" };
 
   return (
-    <div id="home" class="home">
+    <div style={wrapperStyle} id="home" class="home">
     <Card>
       <Card.Img variant='top' src='holder.js/100px180' />
       <Card.Body>
         <Card.Title>Ramiro Peña</Card.Title>
         <Card.Text>Full Stack Developer</Card.Text>
-        <Button  src="public\images\res_test_2.jpg" href="public\images\res_test_2.jpg" variant='primary'>Resume</Button> 
-       
+      <Button>dfg</Button>
       </Card.Body>
     </Card>
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
+import Carousel from 'react-bootstrap/Carousel'
 
 // import { useSelector } from "react-redux"; // gets data from the store
 // import { useDispatch } from "react-redux"; // updates the store data
@@ -21,53 +22,94 @@ export default function Projects() {
   // jsx section
   return (
     <div style={wrapperStyle} id="projects">
-      <h2>PROJECTS</h2>
+      <h2>PROJECTS</h2>  
+
+
+       {/*--------CAROUSEL-------------  */}
+      <Carousel class="carousel">
+        <Carousel.Item interval={5000}>
+          <img
+            className="crossword"
+            src="http://placekitten.com/700/700"
+            alt="yeth"
+          />
+          <Carousel.Caption>
+            <h3>Crossword Puzzle</h3>
+            <p>This is my first solo project. Object was to create a mini game.</p>
+            <a class="proBut1" href="https://90scrossword.netlify.app" >Try my Game</a>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src="http://placekitten.com/700/700"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src="http://placekitten.com/700/700"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      {/* ----------CARDS--------------- */}
       <div class="cards">
       <CardGroup>
         {/* 1ST PROJECT */}
-        <Card>
+        <Card class="bottomCards">
           <Card.Img variant='top' src='holder.js/100px160' />
           <Card.Body>
-            <Card.Title>Crossword Puzzle</Card.Title>
-            <Card.Text>
-              This is my first solo project. Object was to create a mini game.
-            </Card.Text>
-            <a class="proBut1" href="https://90scrossword.netlify.app" >Try my Game</a>
+          <Card.Title>Crossword Puzzle</Card.Title>
+          <Card.Text>
+          This is my first solo project. Object was to create a mini game.
+          </Card.Text>
+          <a class="proBut1" href="https://90scrossword.netlify.app" >Try my Game</a>
           </Card.Body>
           <Card.Footer>
-            <small className='text-muted'>HTML, CSS, Javascript, Deployed on Netlify</small>
+          <small>HTML, CSS, Javascript, Deployed on Netlify</small>
           </Card.Footer>
         </Card>
 
         {/* 2ND PROJECT */}
-        <Card>
+        <Card class="bottomCards">
           <Card.Img variant='top' src='holder.js/100px160' />
           <Card.Body>
-            <Card.Title>Payroll Portal</Card.Title>
-            <Card.Text>This app creates users and generates printable reports</Card.Text>
-            <a class="proBut1" href="https://90scrossword.netlify.app" >Try my Game</a>
+          <Card.Title>Payroll Portal</Card.Title>
+          <Card.Text>This app creates users and generates printable reports</Card.Text>
+          <a class="proBut1" href="https://90scrossword.netlify.app" >Try my Game</a>
           </Card.Body>
           <Card.Footer>
-            <small className='text-muted'>ReactJS, MongoDB, CSS, and Bootstrap</small>
+          <small>ReactJS, MongoDB, CSS, and Bootstrap</small>
           </Card.Footer>
         </Card>
         
         {/* 3RD PROJECT */}
-        <Card>
+          <Card class="bottomCards">
           <Card.Img variant='top' src='holder.js/100px160' />
           <Card.Body>
-            <Card.Title>Marketing Website</Card.Title>
-            <Card.Text>
-              This app allows users to comment, and add items to a shopping cart action.
-            </Card.Text>
-            <a class="proBut1" href="https://90scrossword.netlify.app" >Try my Game</a>
+          <Card.Title>Marketing Website</Card.Title>
+          <Card.Text>
+          This app allows users to comment, and add items to a shopping cart action.
+          </Card.Text>
+          <a class="proBut1" href="https://90scrossword.netlify.app" >Try my Game</a>
           </Card.Body>
           <Card.Footer>
-            <small className='text-muted'>ReactJS, MongoDB, CSS, and Bootstrap</small>
+          <small>ReactJS, MongoDB, CSS, and Bootstrap</small>
           </Card.Footer>
-        </Card>
-      </CardGroup>
+          </Card>
+        </CardGroup>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
